de92bd24e5711092f886eeaf0dc537a2965730ac
suddiyo <suddii01@sju.ac.kr>
2023-02-26 01:53:02 +0900

    [스프링 핵심 원리 - 기본편] section 6

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();   // 동시성 issue 발생할 수 있음 -> 실무에서는 concurrency
