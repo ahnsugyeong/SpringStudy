2e2966390f8a21f3be736f6d65c1eed9ec38354f
suddiyo <suddii01@sju.ac.kr>
2023-03-30 15:59:51 +0900

    [JPA 활용1-(1)] section 2

---
+++ JPA 활용 1-(1)/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;  // FK
+
+    // JPQL select o from order o; -> SQL select * from order. 100 + 1 (order)
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)  // OrderItem Table의 order에 의해 mapping 됨
+    private List<OrderItem> orderItems;
+
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
+
+    //== 연관관계 편의 메서드 (양방향) ==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+}
