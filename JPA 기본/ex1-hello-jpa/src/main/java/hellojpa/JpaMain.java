7dfdeddf78fbe99c3fec07c4009e811c49b529fe
suddiyo <suddii01@sju.ac.kr>
2023-04-22 02:59:27 +0900

    [JPA 기본] section 9

--- JPA 기본/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA 기본/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -6,8 +6,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.time.LocalDateTime;
-import java.util.List;
 
 public class JpaMain {
 
@@ -18,63 +16,36 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Team team = new Team();
-//            team.setName("teamA");
-//            em.persist(team);
+            // 값 타입 -> 불변 객체! (setter private으로 설정)
+            Address address = new Address("homeCity", "street", "10000");
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(address);
+//            em.persist(member);
 //
-//            Member member1 = new Member();
-//            member1.setUsername("member1");
-//            member1.setTeam(team);
-//            em.persist(member1);
-//
-//
-//            em.flush();
-//            em.clear();
-
-            // 프록시
-//            Member refMember = em.getReference(Member.class, member1.getId());
-//            System.out.println("refMember = " + refMember.getClass());  // Proxy
-
-//            em.detach(refMember);
-//            em.close();
-//            em.clear();
-
-//            refMember.getUsername();
-
-//            Member findMember = em.find(Member.class, member1.getId());
-//            System.out.println("findMember = " + findMember.getClass());    // Proxy
-//
-//            System.out.println("refMember == findMember: " + (refMember == findMember));    // ⭐️ JPA에서 이 식이 성립해야 함 !!!!
-
-            // PersistenceUnitUtil
-//            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
-//            Hibernate.initialize(refMember);    // 강제 초기화
+//            Address newAddress = new Address("newCity", "street", "10000");
+//            member.setHomeAddress(newAddress);
 
 
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("피자");
+            member.getFavoriteFoods().add("족발");
 
-            ////////////////////////////////////////////////////////
-            // 즉시 로딩과 지연 로딩
-//            Member m = em.find(Member.class, member1.getId());
-//            System.out.println("m = " + m.getTeam().getClass());
-//
-//            m.getTeam().getName();
+//            member.getAddressHistory().add(new Address("old1", "street", "10000"));
+//            member.getAddressHistory().add(new Address("old2", "street", "10000"));
 
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
 
-            // 영속성 전이(CASCADE)와 고아 객체
-            Child child1 = new Child();
-            Child child2 = new Child();
-
-            Parent parent = new Parent();
-            parent.addChild(child1);
-            parent.addChild(child2);
-
-            em.persist(parent);
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            Parent findParent = em.find(Parent.class, parent.getId());
-            findParent.getChildList().remove(0);
+            System.out.println("==========");
+            Member findMember = em.find(Member.class, member.getId());
+
 
             tx.commit();
         } catch (Exception e) {
@@ -84,17 +55,6 @@ public class JpaMain {
             em.close();
         }
         emf.close();
-    }
-
-    private static void printMember(Member member) {
-        System.out.println("username = " + member.getUsername());
-    }
-    private static void printMemberAndTeam(Member member) {
-        String username = member.getUsername();
-        System.out.println("username = " + username);
-
-        Team team = member.getTeam();
-        System.out.println("team = " + team.getName());
     }
 
 }
--- JPA 기본/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ JPA 기본/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,13 +2,11 @@ package hellojpa;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.locks.Lock;
 
 @Entity
-public class Member extends BaseEntity {
+public class Member{
     @Id
     @GeneratedValue
     @Column(name = "MEMBER_ID")
@@ -17,9 +15,27 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
+    // 주소 Address
+    @Embedded
+    private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns =
+//        @JoinColumn(name = "MEMBER_ID")
+//    )
+//    private List<Address> addressHistory = new ArrayList<>();
+
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -37,11 +53,28 @@ public class Member extends BaseEntity {
         this.username = username;
     }
 
-    public Team getTeam() {
-        return team;
+
+    public Address getHomeAddress() {
+        return homeAddress;
     }
 
-    public void setTeam(Team team) {
-        this.team = team;
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
