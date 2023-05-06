f56bd38e7a7d9063fa3d06bcfa292f5e90223239
suddiyo <suddii01@sju.ac.kr>
2023-05-07 02:27:29 +0900

    [JPA 활용2] section 6

--- JPA 활용 2/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ JPA 활용 2/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,9 +1,11 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
-import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -12,12 +14,20 @@ import org.springframework.util.StringUtils;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jpabook.jpashop.domain.QMember.*;
+import static jpabook.jpashop.domain.QOrder.*;
+
 
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
 
     public void save(Order order) {
         em.persist(order);
@@ -116,5 +126,28 @@ public class OrderRepository {
                 ).setFirstResult(offset)
                 .setMaxResults(limit)
                 .getResultList();
+    }
+
+    /**
+     * QueryDSL 사용
+     */
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) return null;
+        return order.status.eq(statusCond);
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if (!StringUtils.hasText(memberName)) return null;
+        return member.name.like(memberName);
     }
 }
--- JPA 활용 2/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ JPA 활용 2/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -41,13 +41,13 @@ public class MemberService {
     }
 
     public Member findOne(Long memberId) {
-        return memberRepository.findOne(memberId);
+        return memberRepository.findById(memberId).get();
     }
 
     @Transactional
     public void update(Long id, String name) {
         // 변경 감지
-        Member member = memberRepository.findOne(id);
+        Member member = memberRepository.findById(id).get();
         member.setName(name);
     }
 }
