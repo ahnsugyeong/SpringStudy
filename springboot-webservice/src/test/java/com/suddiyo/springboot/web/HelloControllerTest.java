9f915d3a3a0461b8dd2639a7f0567371122a8e36
suddiyo <suddii01@sju.ac.kr>
2023-06-18 17:00:19 +0900

    Chapter 02. 스프링 부트에서 테스트 코드를 작성하자

---
+++ springboot-webservice/src/test/java/com/suddiyo/springboot/web/HelloControllerTest.java
@@ -0,0 +1,43 @@
+package com.suddiyo.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)    // Web(Spring MVC)에 집중할 수 있는 어노테이션. @Controller, @ControllerAdvice 사용 가능, @Service, @Component 등 사용 불가능. 컨트롤러만 사용할 때 선언
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;    // API test에 사용. 스프링 MVC 테스트의 시작점
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount))); // JSON 응답값을 필드별로 검증할 수 있는 메소드
+
+    }
+}
