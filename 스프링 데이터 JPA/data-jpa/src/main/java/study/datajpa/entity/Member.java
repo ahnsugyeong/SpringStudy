d31baa26eda623fed513376dafcb1ffead7050c9
suddiyo <suddii01@sju.ac.kr>
2023-07-08 20:12:46 +0900

    [스프링 데이터 JPA] section 4

--- 스프링 데이터 JPA/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ 스프링 데이터 JPA/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -3,10 +3,16 @@ package study.datajpa.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
+
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
+@NamedQuery(
+        name="Member.findByUsername",
+        query="select m from Member m where m.username = :username"
+)
+@NamedEntityGraph(name = "Member.all", attributeNodes = @NamedAttributeNode("team"))
+@Entity
 public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
@@ -20,6 +26,11 @@ public class Member {
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
     }
 
     public Member(String username, int age, Team team) {
